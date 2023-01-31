30768497edee830e877e83e063b43f2262f96d96
Gabrielparizet <parizetgabriel@gmail.com>
2023-01-31 18:45:26 +0100

    (feat) change Python version locally

---
+++ python/find_your_vilain_name.py
@@ -0,0 +1,15 @@
+def get_villain_name(birthdate): 
+    first = [ "The Evil","The Vile","The Cruel", "The Trashy","The Despicable", "The Embarrassing", "The Disreputable","The Atrocious", "The Twirling",  "The Orange","The Terrifying", "The Awkward"]
+    last = ["Mustache", "Pickle", "Hood Ornament", "Raisin", "Recycling Bin", "Potato", "Tomato", "House Cat", "Teaspoon", "Laundry Basket"]
+    birthdate = birthdate.split("/")
+    month = int(birthdate[0])
+    day = int(birthdate[1])
+    match month:
+        case 1:
+            first:
+
+    
+
+
+
+get_villain_name("1/4/1994")
