a5fca7ef663c5d6e8341b16e381df4029cd480c5
Gabrielparizet <parizetgabriel@gmail.com>
2023-02-07 18:50:01 +0100

    add new exercises

---
+++ js/fizzbuss.js
@@ -0,0 +1,17 @@
+const fizzBuzz = (N) => {
+    resultArray = [];
+    for (i = 1; i <= N; i++){
+        if (i % 3 == 0 && i % 5 == 0){
+            resultArray.push("FizzBuzz");
+        } else if (i % 3 == 0){
+            resultArray.push("Fizz");
+        } else if (i % 5 == 0){
+            resultArray.push("Buzz");
+        } else {
+            resultArray.push(i);
+        }
+    }
+    return resultArray;
+}
+
+fizzBuzz(100);
